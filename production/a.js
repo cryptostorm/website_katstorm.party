0205c16c04ecfb7aaa6059059b3b1a048372950e
Pattern Juggled ie ðørkßöt <pj@cryptostorm.is>
2015-08-02 17:59:26 +0200

    long overdue updates to insert text

--- production/a.js
+++ production/a.js
@@ -1,9 +1,9 @@
 function cca() {
  var tA = [
-  'keep safe & secure on the <a href="https://cryptostorm.is/">cryptostorm network</a>... and keep your filthy pr0n viewing habits private, eh?  ;)',
-  'Does your mother know you just did a search for that torrent? Hop on <a href="https://cryptostorm.is/">cstÃ¸rmË£Ë£</a> and make sure she need not, you cheeky monkey you!  ;)',
-  '<a href="https://cryptostorm.is/">cstÃ¸rmË£Ë£</a> ~ protecting hardworking Canadians whilst online, when you go off searching for moose porn or whatever else Canadians watch  ;)',
-  'Brought to you by <a href="https://cryptostorm.is/">cryptostorm</a> - real network security, no useless bullshit... well not when it comes to privacy tech anyhow...'
+  '<a href="https://cryptostorm.is" target=_"blank">keep safe & secure on the cryptostorm private network</a>... & keep your Rule 34-transcending pr0n viewing habits private. Please, seriously, because... table pr0n?  ;-)',
+  'does your mother know you just did a search for that torrent? <a href="https://cryptostorm.is">snuggle up cosy & secure inside the çrÿþtøstörmðårkŋêt</a> and make sure she need not, you cheeky monkey you!  ;)',
+  '<a href="https://cryptostorm.is" target ="_blank">ÇℜϒΡΤΘŠΤΘℜΜ  - because the  internet is  full of mean people who hate filesharing and will send evil mojo your way if you dare share files, so eeek do not be that guy who gets stalked by creepy media monopolists, phew!</a>
+  'brought to you by <a href="https://cryptostorm.is" target="_blank">cryptostorm, a team delivering legit network security, and none of the usual useless "VPN service" bullshit... also no malware hiding in "VPN" software, which happens alot</a>.
  ];
  var rN = Math.floor(Math.random()*tA.length);
  document.getElementById("cA").innerHTML='<div style="width:100%;height:32px;line-height:32px;background:yellow;color:black;vertical-align: middle;text-align: center;">' + tA[rN] + '</div><div style="height:8px">&nbsp;</div>';
