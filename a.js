3d40967177fcf065cf55586bab47981db6f40d0d
Pattern Juggled ie ðørkßöt <pj@cryptostorm.is>
2015-08-02 08:53:33 +0200

    Create a.js
    source: 
    https://katstorm.party/a.js

---
+++ a.js
@@ -0,0 +1,10 @@
+function cca() {
+ var tA = [
+  'keep safe & secure on the <a href="https://cryptostorm.is/">cryptostorm network</a>... and keep your filthy pr0n viewing habits private, eh?  ;)',
+  'Does your mother know you just did a search for that torrent? Hop on <a href="https://cryptostorm.is/">cstÃ¸rmË£Ë£</a> and make sure she need not, you cheeky monkey you!  ;)',
+  '<a href="https://cryptostorm.is/">cstÃ¸rmË£Ë£</a> ~ protecting hardworking Canadians whilst online, when you go off searching for moose porn or whatever else Canadians watch  ;)',
+  'Brought to you by <a href="https://cryptostorm.is/">cryptostorm</a> - real network security, no useless bullshit... well not when it comes to privacy tech anyhow...'
+ ];
+ var rN = Math.floor(Math.random()*tA.length);
+ document.getElementById("cA").innerHTML='<div style="width:100%;height:32px;line-height:32px;background:yellow;color:black;vertical-align: middle;text-align: center;">' + tA[rN] + '</div><div style="height:8px">&nbsp;</div>';
+}
